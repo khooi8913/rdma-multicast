4a566116828f30885605f72b2bea7528ba5c44f0
Xin Zhe Khooi <khooi8913@gmail.com>
2021-10-20 23:24:15 +0800

    updated ns_setup

--- ns_setup.sh
+++ ns_setup.sh
@@ -27,6 +27,10 @@ sudo ip netns exec ns3 ip link set lo up
 sudo ip netns exec ns3 ip link set ens2f0 up
 sudo ip netns exec ns3 ip addr add 10.10.10.3/24 dev ens2f0
 sudo ip netns exec ns3 ip a
+sudo ip netns exec ns3 arp -s 10.10.10.1 b8:ce:f6:04:6b:d0
+sudo ip netns exec ns3 arp -s 10.10.10.2 b8:ce:f6:04:6b:d1
+sudo ip netns exec ns3 arp -s 10.10.10.4 b8:ce:f6:04:6c:05
+
 
 
 sudo ip netns add ns4
@@ -35,5 +39,9 @@ sudo ip netns exec ns4 ip link set lo up
 sudo ip netns exec ns4 ip link set ens2f1 up
 sudo ip netns exec ns4 ip addr add 10.10.10.4/24 dev ens2f1
 sudo ip netns exec ns4 ip a
+sudo ip netns exec ns4 arp -s 10.10.10.1 b8:ce:f6:04:6b:d0
+sudo ip netns exec ns4 arp -s 10.10.10.2 b8:ce:f6:04:6b:d1
+sudo ip netns exec ns4 arp -s 10.10.10.3 b8:ce:f6:04:6c:04
+
 
 
